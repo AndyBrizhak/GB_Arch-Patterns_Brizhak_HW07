28277d4234501b04756a180afcec30606eed7459
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-19 06:18:15 +0800

    public interface IShippingStrategy

---
+++ GB_Arch+Patterns_Brizhak_HW07_01/IShippingStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_Arch_Patterns_Brizhak_HW07_01
+{
+    public interface IShippingStrategy
+    {
+        double Calculate(Order order);
+    }
+}
