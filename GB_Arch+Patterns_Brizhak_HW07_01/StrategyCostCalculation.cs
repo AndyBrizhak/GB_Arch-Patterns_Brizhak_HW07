c0e1968c9cc5deb9c546a76d2436f34a558a5f5d
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-19 06:45:46 +0800

    +public class StrategyCostCalculation

---
+++ GB_Arch+Patterns_Brizhak_HW07_01/StrategyCostCalculation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_Arch_Patterns_Brizhak_HW07_01
+{
+    public class StrategyCostCalculation
+    {
+        private readonly IShippingStrategy _strategyShipping;
+
+        public StrategyCostCalculation(IShippingStrategy strategyShipping)
+        {
+            _strategyShipping = strategyShipping ?? throw new ArgumentNullException(nameof(strategyShipping));
+        }
+
+        public double CalculateShippingCost(Order order)
+        {
+            return _strategyShipping.Calculate(order);
+        }
+    }
+}
