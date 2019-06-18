0a11b1ca6694500b1fc3c0052b837d191c796e2b
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-19 06:31:18 +0800

    public class UpsStrategy

---
+++ GB_Arch+Patterns_Brizhak_HW07_01/UpsStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_Arch_Patterns_Brizhak_HW07_01
+{
+    public class UpsStrategy : IShippingStrategy
+    {
+        public double Calculate(Order order) => 4;
+    }
+}
