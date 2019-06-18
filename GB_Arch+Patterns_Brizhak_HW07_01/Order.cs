5b20bff4cb21b3591ecaaf168453e7d028a26002
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-19 06:00:43 +0800

    + public class Order

---
+++ GB_Arch+Patterns_Brizhak_HW07_01/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_Arch_Patterns_Brizhak_HW07_01
+{
+    public class Order
+    {
+        public ShippingOptions ShippingMethod { get; set; }
+
+        public Address Destination { get; set; }
+
+        public Address Origin { get; set; }
+    }
+}
