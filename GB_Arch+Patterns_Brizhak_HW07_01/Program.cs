81d668852c80021bbbf6cd74ecfcce2011c098b3
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-18 19:43:42 +0800

    +

--- GB_Arch+Patterns_Brizhak_HW07_01/Program.cs
+++ GB_Arch+Patterns_Brizhak_HW07_01/Program.cs
@@ -4,6 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// 1. Реализовать шаблон «Стратегия» без использования делегатов.
+// При решении задачи следует
+// использовать следующий интерфейс:
+//public interface IShippingStrategy
+//{
+//    double Calculate(Order order);
+//}
+
+
 namespace GB_Arch_Patterns_Brizhak_HW07_01
 {
     class Program
